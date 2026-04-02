f8deb02a1cd6bdd807918522442b2962675c59bf
nicole <sodevious.net@gmail.com>
2026-04-02 09:18:36 -0400

    add more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,34 +31,6 @@ module.exports = {
           '900': '#0F165A',
           '950': '#090E3B',
         },
-        'orange': {
-          '50': '#FFF2F0',
-          '100': '#FFDDD9',
-          '200': '#FFB8B0',
-          '300': '#FF9285',
-          '400': '#FF6752',
-          DEFAULT: '#FF6752',
-          '500': '#F03D29',
-          '600': '#CC2718',
-          '700': '#A31E12',
-          '800': '#7A140C',
-          '900': '#520D07',
-          '950': '#330804',
-        },
-        'yellow': {
-          '50': '#FFFAEF',
-          '100': '#FFF0CC',
-          '200': '#FFE0A0',
-          '300': '#FFBC5E',
-          DEFAULT: '#FFBC5E',
-          '400': '#FFA020',
-          '500': '#E87D00',
-          '600': '#CC6C00',
-          '700': '#A35500',
-          '800': '#7A3F00',
-          '900': '#502800',
-          '950': '#331900',
-        },
         'dark-blue': {
           '50': '#EEF1FC',
           '100': '#D4DCFA',
@@ -87,7 +59,12 @@ module.exports = {
           DEFAULT: '#222228',
           '950': '#141418',
         },
-        'secondary': '#BBBDC1'
+        'yellow': '#FFBC5E',
+        'orange': '#FF6752',
+        'primary': '#9CB2FF',
+        'secondary': '#BBBDC1',
+        'background': '#ECEFFC',
+        'stroke': '#E6EBF2'
       },
       fontSize: {
         '5xl': ['3rem', 1.1]
